6a5438ba6ef53e4ab9f0d9794bd687fed955d638
VickyramPatel <vickyrampatel@outlook.com>
2025-04-30 10:40:53 +0530

    Java code version 2

--- Main.java
+++ Main.java
@@ -1,1 +1,1 @@
-public class Main { public static void main(String[] args) { System.out.println("Version 1"); } }
+public class Main { public static void main(String[] args) { System.out.println("Version 2"); } }
